bf18f181e936ad3095790198361f23812c0c5c95
Your Name <OzyKartike@users.noreply.github.com>
2025-01-21 20:27:06 -0500

    finished code

--- DailyLC/01_2025/Jan_21/sol_235.py
+++ DailyLC/01_2025/Jan_21/sol_235.py
@@ -1,3 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 # basic strat check if root.val > low the go right
 # if root.val < high then go left
 # if root.val is in between low and high then return root.val
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return None
+        high = max(p.val, q.val)
+        low = min(p.val, q.val)
+        if root.val < low:
+            return self.lowestCommonAncestor(root.right, p, q)
+        if root.val > high:
+            return self.lowestCommonAncestor(root.left, p, q)
+        return root
